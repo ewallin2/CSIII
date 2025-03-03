2c4eff266d4c5d90d3833d0575acbf32d426a806
ewallin2 <25wallin@friendsseminary.org>
2025-03-03 05:24:27 +0000

    exercise8

--- Homework3/exercise8.py
+++ Homework3/exercise8.py
@@ -1,1 +1,17 @@
+sent_message = 'Hey there! This is a secret message.'
+
+with open('sent_message.txt', 'w') as file:
+  file.write(sent_message)
+
+with open('sent_message.txt', 'r+') as file:
+  # Read the sent message from the file
+  original_message = file.read()
+      
+  # Move the cursor to the beginning of the file
+  file.seek(0)
+
+  # Modify the message to simulate unsending
+  file.truncate()
+  unsent_message = 'This message has been unsent.'
+  file.write(unsent_message)
 
