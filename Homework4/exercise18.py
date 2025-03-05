3b8fc8e8af532977e8b039ce03b46033cb6fabce
ewallin2 <25wallin@friendsseminary.org>
2025-03-05 20:07:40 +0000

    exercises 18,19,20

--- Homework4/exercise18.py
+++ Homework4/exercise18.py
@@ -1,1 +1,30 @@
+import unittest
+import math
+
+def get_sqrt(n):
+  return math.sqrt(n)
+
+def divide(a, b):
+  return a / b
+
+class TestMath(unittest.TestCase):
+    def test_sqrt(self):
+        inp = 144
+        expected_result = 12
+        self.assertEqual(get_sqrt(inp),expected_result)
+        
+        with self.assertRaises(TypeError):
+            get_sqrt("a")
+
+    def test_divide(self):
+       inp1 = 4
+       inp2 = 2
+       expected_result = 2
+       self.assertEqual(divide(inp1, inp2), 2)
+
+       with self.assertRaises(ZeroDivisionError):
+          divide(1,0)
+          
+if __name__ == '__main__':
+  unittest.main()
 
--- Homework4/exercise20.py
+++ Homework4/exercise20.py
@@ -1,1 +1,46 @@
+import unittest
+
+class CoffeeMenu:
+    def __init__(self):
+        self.menu = {
+            'espresso': 2.50,
+            'latte': 2.75,
+            'cappuccino': 3.20,
+            'americano': 2.70
+        }
+
+    def get_price(self, item):
+        return self.menu[item]
+
+    def add_item(self, item, price):
+        if item not in self.menu:
+            self.menu[item] = price
+
+
+
+
+class TestCoffeeMenu(unittest.TestCase):
+    def setUp(self):
+        self.coffee_menu = CoffeeMenu()
+
+    def test_get_price_existing_item(self):
+        self.assertEqual(self.coffee_menu.get_price('espresso'), 2.50)
+
+    def test_get_price_non_existing_item(self):
+        with self.assertRaises(KeyError):
+            self.coffee_menu.get_price('mocha')
+
+    def test_add_item(self):
+        self.coffee_menu.add_item('mocha', 3.50)
+        self.assertEqual(self.coffee_menu.get_price('mocha'), 3.50)
+
+    def test_add_existing_item(self):
+        result = self.coffee_menu.add_item('latte', 3.00)
+        self.assertFalse(result)
+        self.assertEqual(self.coffee_menu.get_price('latte'), 2.75)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 
